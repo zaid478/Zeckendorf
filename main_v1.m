40e11b2e79625fb0ba52e507a3fbd0cc54306e6e
Muhammad Zaid <mzaid7478@gmail.com>
2018-07-30 13:26:26 +0500

    Update main_v1.m

--- main_v1.m
+++ main_v1.m
@@ -3,11 +3,6 @@ function Z1=main_v1(image,operation,mode)
 
 [zecklist,map2]=binpattern();
 
-
-
-% gImg=rgb2gray(image);
-% s=fspecial('gaussian');
- %img=imfilter(gImg,s);
  if (mode == 'g')
  [Z,Z1,Z2]=zeck_check(image,zecklist,operation);
 
